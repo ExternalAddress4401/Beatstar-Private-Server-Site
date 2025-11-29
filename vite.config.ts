04b7a1c9ce861adae8fd4dc4e9466edaaef25c75
ExternalAddress4401 <external_address@hotmail.com>
2025-11-28 19:07:50 -0800

    Add dev domain

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,11 @@ import { defineConfig } from 'vite';
 export default defineConfig({
 	plugins: [sveltekit()],
 	server: {
-		allowedHosts: ['www.beatstarmod.app', 'beatstarmod.app']
+		allowedHosts: [
+			'www.beatstarmod.app',
+			'beatstarmod.app',
+			'www.dev.beatstarmod.app',
+			'dev.beatstarmod.app'
+		]
 	}
 });
